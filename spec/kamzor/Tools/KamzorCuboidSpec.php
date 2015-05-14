0e58906e5850dc621253f27c05e575375969236e
Kamzor <kkamil_jurekk@o2.pl>
2015-05-14 12:23:53 +0200

    KamzorCuboid

--- spec/kamzor/Tools/KamzorCuboidSpec.php
+++ spec/kamzor/Tools/KamzorCuboidSpec.php
@@ -11,4 +11,16 @@ class KamzorCuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kamzor\Tools\KamzorCuboid');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+        $this->setB(2)->getB()->shouldReturn(2);
+        $this->setH(4)->getH()->shouldReturn(4);
+    }
+    
+    function it_should_calculate_objetosc()
+    {
+        $this->setA(2)->setB(2)->setH(4)->objetosc()->shouldReturn(16);
+    }
 }
